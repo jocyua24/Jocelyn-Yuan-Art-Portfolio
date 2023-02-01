ed8dba3d974ade29ca51101557efdf6c2c86cb0e
Jocelyn <jocyua24@bergen.org>
2023-02-01 10:43:18 -0500

    add another art piece

--- modal.js
+++ modal.js
@@ -133,3 +133,16 @@ img.onclick = function(){
   captionText.innerHTML = this.alt;
 }
 
+// Blossoms
+var modal = document.getElementById("myModal");
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.getElementById("myImg9"); //only change this
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+img.onclick = function(){
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+}
+
