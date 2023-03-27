f077d3f1834afc218f354b44e8699f0f26cd55aa
Jocelyn <jocyua24@bergen.org>
2023-03-27 14:33:33 -0400

    fish modal

--- modal.js
+++ modal.js
@@ -185,3 +185,15 @@ img.onclick = function(){
   captionText.innerHTML = this.alt;
 }
 
+// fish
+var modal = document.getElementById("myModal");
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.getElementById("myImg13"); //only change this
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+img.onclick = function(){
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+}
