f0fa94664fe762228865adb6d8218dd5b4b4119a
Jocelyn <jocyua24@bergen.org>
2023-10-18 01:26:48 -0400

    new page

--- sprite.js
+++ sprite.js
@@ -1,4 +1,4 @@
-const PAGES = ['index', 'gallery', 'game', 'contact'];
+const PAGES = ['index', 'gallery', 'game', 'Taro', 'contact'];
 const canvas = document.getElementById('canvas1');
 const ctx = canvas.getContext('2d');
 const CANVAS_WIDTH = canvas.width = 600;
